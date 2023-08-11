0ff2e131184c852039214b9748caa3688c19e081
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-08-11 11:56:46 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/perpetrator_manangement_system/Perpetrator_Manangement_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perpetrator_manangement_system;
+
+/**
+ *
+ * @author Shehan
+ */
+public class Perpetrator_Manangement_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
