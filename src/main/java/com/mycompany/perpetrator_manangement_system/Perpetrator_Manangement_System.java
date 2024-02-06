4ce2235c07b34b1a8566f61e0a2afdec477ea0f5
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-02-06 20:54:48 +0530

    login initialization and UI

--- src/main/java/com/mycompany/perpetrator_manangement_system/Perpetrator_Manangement_System.java
+++ src/main/java/com/mycompany/perpetrator_manangement_system/Perpetrator_Manangement_System.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.perpetrator_manangement_system;
 
-/**
- *
- * @author Shehan
- */
 public class Perpetrator_Manangement_System {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LoginActivity loginActivity = new LoginActivity();
+        loginActivity.setVisible(true);
     }
 }
