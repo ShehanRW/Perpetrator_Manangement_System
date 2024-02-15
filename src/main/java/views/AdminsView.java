e1e94204dd9d51a86dc3d7d622f7726b77cc507f
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-02-16 00:21:11 +0530

    admin panel ui

---
+++ src/main/java/views/AdminsView.java
@@ -0,0 +1,221 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package views;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class AdminsView extends javax.swing.JFrame {
+
+    public AdminsView() {
+        initComponents();
+        
+         //Full Screen
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
+        Color primaryColor = new Color(0,11,40);
+        getContentPane().setBackground(primaryColor);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        back_btn = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        adminName = new javax.swing.JLabel();
+        position = new javax.swing.JLabel();
+        adminType = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        back_btn.setBackground(new java.awt.Color(0, 2, 69));
+        back_btn.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        back_btn.setForeground(new java.awt.Color(119, 155, 255));
+        back_btn.setText("Back");
+        back_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                back_btnActionPerformed(evt);
+            }
+        });
+
+        jSeparator1.setBackground(new java.awt.Color(0, 2, 69));
+        jSeparator1.setForeground(new java.awt.Color(119, 155, 255));
+
+        jLabel1.setFont(new java.awt.Font("Bahnschrift", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(119, 155, 255));
+        jLabel1.setText("Perpetraitor Database");
+        jLabel1.setAlignmentX(0.5F);
+
+        jLabel2.setFont(new java.awt.Font("Bahnschrift", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(119, 155, 255));
+        jLabel2.setText("Admin Panel");
+        jLabel2.setAlignmentX(0.5F);
+
+        jSeparator2.setBackground(new java.awt.Color(0, 2, 69));
+        jSeparator2.setForeground(new java.awt.Color(119, 155, 255));
+        jSeparator2.setOrientation(javax.swing.SwingConstants.VERTICAL);
+
+        jLabel3.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(119, 155, 255));
+        jLabel3.setText("Current user details");
+        jLabel3.setAlignmentX(0.5F);
+
+        jLabel4.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(119, 155, 255));
+        jLabel4.setText("Name");
+        jLabel4.setAlignmentX(0.5F);
+
+        jLabel5.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(119, 155, 255));
+        jLabel5.setText("Position");
+        jLabel5.setAlignmentX(0.5F);
+
+        jLabel6.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(119, 155, 255));
+        jLabel6.setText("Type");
+        jLabel6.setAlignmentX(0.5F);
+
+        adminName.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        adminName.setForeground(new java.awt.Color(119, 155, 255));
+        adminName.setAlignmentX(0.5F);
+
+        position.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        position.setForeground(new java.awt.Color(119, 155, 255));
+        position.setAlignmentX(0.5F);
+
+        adminType.setFont(new java.awt.Font("Bahnschrift", 1, 12)); // NOI18N
+        adminType.setForeground(new java.awt.Color(119, 155, 255));
+        adminType.setAlignmentX(0.5F);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(72, 72, 72)
+                        .addComponent(jLabel2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jSeparator1)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel6))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(adminName)
+                                    .addComponent(position)
+                                    .addComponent(adminType))))))
+                .addGap(25, 25, 25)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 1021, Short.MAX_VALUE)
+                .addComponent(back_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel5)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel6))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(adminName)
+                        .addGap(18, 18, 18)
+                        .addComponent(position)
+                        .addGap(18, 18, 18)
+                        .addComponent(adminType)))
+                .addContainerGap(700, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(back_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
+            .addComponent(jSeparator2)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void back_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_back_btnActionPerformed
+        new Menu().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_back_btnActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AdminsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AdminsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AdminsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AdminsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AdminsView().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel adminName;
+    private javax.swing.JLabel adminType;
+    private javax.swing.JButton back_btn;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JLabel position;
+    // End of variables declaration//GEN-END:variables
+}
