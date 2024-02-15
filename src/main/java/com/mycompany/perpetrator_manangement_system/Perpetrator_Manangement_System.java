f55d2de5eb2cc9a3e428dccecc554ca1c10de785
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-02-16 00:43:34 +0530

    Database connection

--- src/main/java/com/mycompany/perpetrator_manangement_system/Perpetrator_Manangement_System.java
+++ src/main/java/com/mycompany/perpetrator_manangement_system/Perpetrator_Manangement_System.java
@@ -1,6 +1,7 @@
 
 package com.mycompany.perpetrator_manangement_system;
 
+import database_controllers.DatabaseConnection;
 import views.LoginActivity;
 
 public class Perpetrator_Manangement_System {
@@ -8,5 +9,7 @@ public class Perpetrator_Manangement_System {
     public static void main(String[] args) {
         LoginActivity loginActivity = new LoginActivity();
         loginActivity.setVisible(true);
+        
+        DatabaseConnection connect = new DatabaseConnection();
     }
 }
--- src/main/java/database_controllers/DatabaseConnection.java
+++ src/main/java/database_controllers/DatabaseConnection.java
@@ -1,5 +1,29 @@
 package database_controllers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DatabaseConnection {
+
+    private String url = "jdbc:mysql://localhost:3306/perpetratordb";
+    private String user = "root";
+    private String password = "";
+    
+    public DatabaseConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connetction = DriverManager.getConnection(url, user, password);
+            System.out.println("Database connected...!");
+        } 
+        catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
